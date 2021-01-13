73ba3cbce3643d85df3dbba7ae71d2c24988d7a6
Maurice frank <mfrank@hb-dx.com>
2021-01-13 18:52:48 +0100

    Fixed Install script

--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 envname=autoprognosis
+source $HOME/.bashrc
 
 # Remove the hbdx enviroment if already installed
 if conda env list | grep -q $envname; then
@@ -10,6 +11,7 @@ fi
 
 # Create a new enviroment called hbdx
 conda create -n $envname python=3.7 -y
+source $HOME/.bashrc
 conda activate $envname
 
 conda env list
@@ -18,11 +20,11 @@ conda env list
 conda install mamba -c conda-forge -y
 
 # Install tensorflow
-mamba install tensorflow-gpu keras -c anaconda
+mamba install tensorflow-gpu keras -c anaconda -y
 
 # Install python stuff from forge
 mamba install pandas gpyopt xgboost sets scikit-learn scipy pivottablejs rpy2 matplotlib tqdm requests jupyter seaborn -c conda-forge -y
 
 # Install r stuff from forge
-mamba install r-essentials r-base r-missForest r-glmnet r-ranger r-xmisc r-domc r-doxboost r-randomforestsrc r-party r-softimpute r-coin -c conda-forge -y
+mamba install r-essentials r-base r-missForest r-glmnet r-ranger r-xmisc r-domc r-coxboost r-randomforestsrc r-party r-softimpute r-coin -c conda-forge -y
 
